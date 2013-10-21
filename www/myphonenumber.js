79a38bceda9cc9b13500ebd579945379d5c353d8
smithleedon <saucebox@nate.com>
2013-10-21 15:16:35 +0900

    hi

--- www/myphonenumber.js
+++ www/myphonenumber.js
@@ -1,15 +1,65 @@
-var myphonenumber = {
-    createEvent: function(mynumber, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback, // success callback function
-            errorCallback, // error callback function
-            'MyPhoneNumber', // mapped to our native Java class called "CalendarPlugin"
-            'getphonenumber', // with this action name
-            [{                  // and this array of custom arguments to create our entry
-                "mynumber": mynumber
-               
-            }]
-        ); 
-    }
+cordova.define("com.smithleedon.device", function(require, exports, module) {/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
+var argscheck = require('cordova/argscheck'),
+    channel = require('cordova/channel'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec');
+
+channel.createSticky('onCordovaInfoReady');
+// Tell cordova channel to wait on the CordovaInfoReady event
+channel.waitForInitialization('onCordovaInfoReady');
+
+/**
+ * This represents the mobile device, and provides properties for inspecting the model, version, UUID of the
+ * phone, etc.
+ * @constructor
+ */
+function MyPhoneNumber() {
+    this.mynumber = null;
+
+    var me = this;
+
+    channel.onCordovaReady.subscribe(function() {
+        me.getInfo(function(info) {
+            me.mynumber = info.mynumber;
+            channel.onCordovaInfoReady.fire();
+        },function(e) {
+            me.available = false;
+            utils.alert("[ERROR] Error initializing Cordova: " + e);
+        });
+    });
 }
-module.exports = myphonenumber;
+
+/**
+ * Get device info
+ *
+ * @param {Function} successCallback The function to call when the heading data is available
+ * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
+ */
+MyPhoneNumber.prototype.getInfo = function(successCallback, errorCallback) {
+    argscheck.checkArgs('fF', 'MyPhoneNumber.getInfo', arguments);
+    exec(successCallback, errorCallback, "getphonenumber", "getDeviceInfo", []);
+};
+
+module.exports = new MyPhoneNumber();
+});
+
